d4f14f12b5792af2bd897f2adc03eb720ae73c7c
Tyler Knox <tyler58546@gmail.com>
2022-04-30 21:59:30 -0700

    username support and other fixes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ class Game {
         return Math.min(6000, Math.floor((Date.now() - this.timeStarted) / 100));
     }
     getState() {
-        return {type: 'state', currentTick: this.getCurrentTick()}
+        return {type: 'state', currentTick: this.getCurrentTick(), usernames: [this.players[0].username, this.players[1].username]}
     }
     onMessage(data, ws) {
         data = JSON.parse(data);
@@ -34,10 +34,19 @@ class Game {
 
 const games  = [];
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
+
+    const sp = new URL('ws://localhost'+req.url).searchParams;
+    if (sp) {
+        ws.username = sp.get('username') || 'No username';
+    } else {
+        ws.username = 'No username';
+    }
+
+    console.log(`${ws.username} connected`);
+
 
     ws.on('message', function message(data) {
-
         for (const game of games) {
             if (game.players.includes(ws)) {
                 game.onMessage(data, ws);
@@ -50,15 +59,18 @@ wss.on('connection', function connection(ws) {
 
     ws.on('close', function close() {
         for (const game of games) {
+            const index = games.indexOf(game);
             for (const player of game.players) {
-                if (game.players < 2) {
+                if (game.players < 2 && player.readyState > 1) {
                     game.players = [];
+                    console.log(`Removed player from game ${index}`)
                 }
             }
             for (const player of game.players) {
                 if (player.readyState < 2) return;
             }
-            games.splice(games.indexOf(game));
+            games.splice(index, 1);
+            console.log(`Closing game ${index}`)
         }
     });
 
@@ -66,6 +78,7 @@ wss.on('connection', function connection(ws) {
         for (const game of games) {
             if (game.players.length < 2) {
                 game.players.push(ws)
+                console.log(`${ws.username} joined game ${games.indexOf(game)} (${game.players.length}/2)`);
                 if (game.players.length > 1) game.start();
                 return;
             }
